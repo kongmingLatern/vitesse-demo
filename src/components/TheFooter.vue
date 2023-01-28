1920d57a315c30e81fda7df1dc1bc4d062486f99
凤之兮原 <1577310596@qq.com>
2023-01-28 20:11:27 +0800

    feat: add prettier and eslint

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,12 +4,7 @@
       <div dark:i-carbon-moon i-carbon-sun />
     </button>
 
-    <a
-      icon-btn i-carbon-logo-github
-      rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
-      target="_blank"
-      title="GitHub"
-    />
+    <a icon-btn i-carbon-logo-github rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank"
+      title="GitHub" />
   </nav>
 </template>
